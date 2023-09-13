8602988db9727c6e3ce031b3b7984b531106b8d1
Mark Johnson <mark.ai.johnson@pm.me>
2023-09-13 17:35:55 +0100

    Remove unused logic
    Pause R10

--- SNES.sv
+++ SNES.sv
@@ -224,7 +224,7 @@ pll pll
 	.rst(0),
 	.outclk_0(clk_mem),
 	.outclk_1(CLK_VIDEO),
-	.outclk_2(clk_sys),
+	.outclk_2(clk_sys), 
 	.reconfig_to_pll(reconfig_to_pll),
 	.reconfig_from_pll(reconfig_from_pll),
 	.locked(clock_locked)
@@ -545,65 +545,20 @@ always @(posedge clk_sys) begin
 end
 
 //////////////////////////////// CE ////////////////////////////////////
-
-
 wire ce;
-// wire cart_act = cart_wr | cart_rd;
-wire cart_act = !ROM_WE_N | (RESET_N ? ~ROM_OE_N : RFSH);
-wire fastforward = joy0[8] && !ioctl_download && !OSD_STATUS;
-wire ff_on;
-wire sdram_refresh_force;
-// wire sleep_savestate;
-
-reg paused;
+wire sdram_busy = !ROM_WE_N | (RESET_N ? !ROM_OE_N : RFSH);
+reg pause;
 always_ff @(posedge clk_sys) begin
-//    paused <= sleep_savestate | (status[26] && OSD_STATUS && !ioctl_download && !reset && ~status[27]); // no pause when downloading rom, resetting or rewind capture is on
-   paused <= (status[47] && OSD_STATUS && !ioctl_download && !reset); // no pause when downloading rom, resetting or rewind capture is on
+	// Block pause when sdram busy, downloading rom or resetting
+   pause <= !sdram_busy && (status[47] && OSD_STATUS && !ioctl_download && !reset); 
 end
 
 core_control core_control
 (
 	.clk_sys     (clk_sys),
-	.pause       (paused),
-	.speedup     (1'b0),
-	.cart_act    (1'b0),
-	.DMA_on      (1'b0),
+	.pause       (pause),
 	.ce          (ce),
-	.refresh     (sdram_refresh_force),
-	.ff_on       (ff_on)
 );
-
-///////////////////////////// Fast Forward Latch /////////////////////////////////
-
-reg fast_forward;
-reg ff_latch;
-
-always @(posedge clk_sys) begin : ffwd
-	reg last_ffw;
-	reg ff_was_held;
-	longint ff_count;
-
-	last_ffw <= fastforward;
-
-	if (fastforward)
-		ff_count <= ff_count + 1;
-
-	if (~last_ffw & fastforward) begin
-		ff_latch <= 0;
-		ff_count <= 0;
-	end
-
-	if ((last_ffw & ~fastforward)) begin // 32mhz clock, 0.2 seconds
-		ff_was_held <= 0;
-
-		if (ff_count < 3200000 && ~ff_was_held) begin
-			ff_was_held <= 1;
-			ff_latch <= 1;
-		end
-	end
-
-	fast_forward <= (fastforward | ff_latch);
-end
 
 ////////////////////////////  SYSTEM  ///////////////////////////////////
 
